4b54e88f963791cf21e576c01296e5139d663e3a
ilya-noize <shumakoff.ilya@yandex.ru>
2023-11-09 00:31:40 +0300

    Unused classes have been removed;

--- src/main/java/ru/practicum/shareit/item/api/repository/ItemStorage.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.item.api.repository;
-
-import ru.practicum.shareit.api.CRUDRepository;
-import ru.practicum.shareit.item.entity.Item;
-
-public interface ItemStorage extends CRUDRepository<Item> {
-}
--- src/main/java/ru/practicum/shareit/user/api/repository/UserStorage.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.user.api.repository;
-
-import ru.practicum.shareit.api.CRUDRepository;
-import ru.practicum.shareit.user.entity.User;
-
-public interface UserStorage extends CRUDRepository<User> {
-}
